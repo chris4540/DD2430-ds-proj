16749dc84bade7cfa876583e327160b1944277fc
Chris Lin <chris4540@gmail.com>
2019-11-13 23:04:40 +0100

    Add plotting

---
+++ utils/plot_fashion_minst.py
@@ -0,0 +1,28 @@
+import numpy as np
+from matplotlib import pyploy as plt
+
+
+class FashionMinstConst:
+    classes = {
+        'T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+        'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot'
+    }
+
+
+class PlotConfig:
+    colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',
+              '#9467bd', '#8c564b', '#e377c2', '#7f7f7f',
+              '#bcbd22', '#17becf']
+
+
+def plot_embeddings(embeddings, targets, xlim=None, ylim=None):
+    plt.figure(figsize=(10, 10))
+    for i in range(10):
+        inds = np.where(targets == i)[0]
+        plt.scatter(embeddings[inds, 0],
+                    embeddings[inds, 1], alpha=0.5, color=PlotConfig.colors[i])
+    if xlim:
+        plt.xlim(xlim[0], xlim[1])
+    if ylim:
+        plt.ylim(ylim[0], ylim[1])
+    plt.legend(FashionMinstConst.classes)
