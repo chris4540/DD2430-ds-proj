9d0a97a96b07fa15d5dda7211b63996b99713a30
Chris Lin <chris4540@gmail.com>
2019-11-14 00:16:25 +0100

    Update siamese

--- model/siamese.py
+++ model/siamese.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2019 Chun Hung Lin, Chris
+Copyright (c) 2019 Chun Hung Lin, Vivek
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -41,3 +41,9 @@ https://github.com/fangpin/siamese-pytorch/blob/5543f1e844964b116dc9d347a5eb164c
 https://github.com/adambielski/siamese-triplet/blob/master/Experiments_FashionMNIST.ipynb
 Refer to my notebooks for required changes to siamese to allign them to the paper
 """
+import torch
+from torchvision.datasets import FashionMNIST
+from torchvision import transforms
+
+if __name__ == '__main__':
+	pass
