64f45619df133e7c5b7cdd56aeca746b9cc5a77a
Chris Lin <chris4540@gmail.com>
2019-11-17 23:53:35 +0100

    Move the template code to code_trunk

--- network/siamese.py
+++ network/siamese.py
@@ -2,12 +2,16 @@ import torch
 import torch.nn as nn
 
 class SiameseNet(nn.Module):
-    def __init__(self, embedding_net):
-        super(SiameseNet, self).__init__()
-        self.embedding_net = embedding_net
+    def __init__(self, emb_net):
+        super().__init__()
+        self._emb_net = emb_net
 
     def forward(self, x):
         x1, x2 = x
-        output1 = self.embedding_net(x1)
-        output2 = self.embedding_net(x2)
-        return output1, output2
+        out1 = self._emb_net(x1)
+        out2 = self._emb_net(x2)
+        return out1, out2
+
+    @property
+    def emb_net(self):
+        return self._emb_net
