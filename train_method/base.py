af3aa3f471462e95152124167181b3e2a3089b9e
Chris Lin <chris4540@gmail.com>
2019-11-18 02:48:54 +0100

    Code refactoring

--- train_method/base.py
+++ train_method/base.py
@@ -1,37 +1,10 @@
-from abc import ABC as AbstractBaseClass
-from abc import abstractmethod
-
 from tqdm import tqdm
 
 
-class TrainingAbstractMethod(AbstractBaseClass):
-
-    @abstractmethod
-    def run(self):
-        pass
-
-    @abstractmethod
-    def prepare_data_loaders(self):
-        """
-        For preparing data loaders and save them as instance attributes
-        """
-        pass
-
-    @abstractmethod
-    def prepare_exp_settings(self):
-        """
-        Define stuff which are before the actual run. For example:
-            - Optimizer
-            - Model
-        """
-        pass
-
-    @abstractmethod
-    def prepare_logging(self):
-        pass
-
-
 class BaseTrainingMethod(AbstractBaseClass):
+    """
+    Basic training method which has a lot of common display settings and callbacks
+    """
 
     # for callback use
 
@@ -67,13 +40,17 @@ class BaseTrainingMethod(AbstractBaseClass):
             initial=0, leave=False, total=train_loader_len,
             desc=self.log_cfg['desc'].format(0)
         )
-
+    # ------------------------
     # Pre-defined callbacks
+    # ------------------------
+    # overide and register to ignite engine if needed
+
     def take_scheduler_step(self, engine):
         self.scheduler.step()
         # Print out
         tqdm.write("Learning Rate - Epoch: {}  Learning Rate: {}"
                    .format(engine.state.epoch, self.scheduler.get_lr()))
+        # lr = self.scheduler.get_lr()[0]
 
     def log_training_loss(self, engine):
         train_loader_len = self.log_cfg['train_loader_len']
@@ -87,7 +64,7 @@ class BaseTrainingMethod(AbstractBaseClass):
             pbar.desc = desc.format(engine.state.output)
             pbar.update(log_interval)
 
-    def log_training_results(self, engine, train_loader, evaluator):
+    def log_training_results(self, engine, evaluator, train_loader):
         pbar = self.log_cfg['pbar']
         # -----------------------------------------------
         pbar.refresh()
@@ -99,3 +76,16 @@ class BaseTrainingMethod(AbstractBaseClass):
             "Training Results - Epoch: {}  Avg accuracy: {:.2f} Avg loss: {:.2f}"
             .format(engine.state.epoch, avg_accuracy, avg_loss)
         )
+
+    def log_validation_results(self, engine, evaluator, val_loader):
+        pbar = self.log_cfg['pbar']
+        # -----------------------------------------------
+        evaluator.run(val_loader)
+        metrics = evaluator.state.metrics
+        avg_accuracy = metrics['accuracy']
+        avg_loss = metrics['loss']
+        tqdm.write(
+            "Validation Results - Epoch: {}  Avg accuracy: {:.2f} Avg loss: {:.2f}"
+            .format(engine.state.epoch, avg_accuracy, avg_loss))
+
+        pbar.n = pbar.last_print_n = 0
--- train_method/baseline.py
+++ train_method/baseline.py
@@ -110,15 +110,15 @@ class BaselineFashionMNIST(BaseTrainingMethod):
         # Alias
         model = self.model
         optimizer = self.optimizer
-        scheduler = self.scheduler
+        # scheduler = self.scheduler
         loss_fn = self.loss_fn
         device = self.device
         eval_metrics = self.eval_metrics
         hparams = self.hparams
         # ----------------------------------
-        log_interval = self.log_cfg['interval']
-        desc = self.log_cfg['desc']
-        pbar = self.log_cfg['pbar']
+        # log_interval = self.log_cfg['interval']
+        # desc = self.log_cfg['desc']
+        # pbar = self.log_cfg['pbar']
         # ----------------------------------
         # Special alias
         train_loader = self.train_loader
@@ -135,49 +135,23 @@ class BaselineFashionMNIST(BaseTrainingMethod):
         trainer.add_event_handler(
             Events.EPOCH_COMPLETED, self.take_scheduler_step)
 
-        # trainer.add_event_handler(
-        #     Events.ITERATION_COMPLETED, self.log_training_loss)
+        trainer.add_event_handler(
+            Events.ITERATION_COMPLETED, self.log_training_loss)
 
-        # trainer.add_event_handler(
-        #     Events.EPOCH_COMPLETED, self.log_training_results, **{
-        #         'train_loader': train_loader,
-        #         'evaluator': evaluator
-        #     })
+        trainer.add_event_handler(
+            Events.EPOCH_COMPLETED, self.log_training_results, **{
+                'train_loader': train_loader,
+                'evaluator': evaluator
+            })
 
-        @trainer.on(Events.ITERATION_COMPLETED)
-        def log_training_loss(engine):
-            iter = (engine.state.iteration - 1) % len(train_loader) + 1
-
-            if iter % log_interval == 0:
-                pbar.desc = desc.format(engine.state.output)
-                pbar.update(log_interval)
-
-        @trainer.on(Events.EPOCH_COMPLETED)
-        def log_training_results(engine):
-            pbar.refresh()
-            evaluator.run(train_loader)
-            metrics = evaluator.state.metrics
-            avg_accuracy = metrics['accuracy']
-            avg_loss = metrics['loss']
-            tqdm.write(
-                "Training Results - Epoch: {}  Avg accuracy: {:.2f} Avg loss: {:.2f}"
-                .format(engine.state.epoch, avg_accuracy, avg_loss)
-            )
-
-        @trainer.on(Events.EPOCH_COMPLETED)
-        def log_validation_results(engine):
-            evaluator.run(val_loader)
-            metrics = evaluator.state.metrics
-            avg_accuracy = metrics['accuracy']
-            avg_loss = metrics['loss']
-            tqdm.write(
-                "Validation Results - Epoch: {}  Avg accuracy: {:.2f} Avg loss: {:.2f}"
-                .format(engine.state.epoch, avg_accuracy, avg_loss))
-
-            pbar.n = pbar.last_print_n = 0
+        trainer.add_event_handler(
+            Events.EPOCH_COMPLETED, self.log_validation_results, **{
+                'val_loader': val_loader,
+                'evaluator': evaluator
+            })
 
         trainer.run(train_loader, max_epochs=hparams.epochs)
-        pbar.close()
+        self.log_cfg['pbar'].close()
 
     def save_model(self):
         pass
