02fb3bf6a33c42504c56164aa7176dcf92430f7a
Chris Lin <chris4540@gmail.com>
2019-11-20 14:59:50 +0100

    Add Siamesize

--- utils/datasets.py
+++ utils/datasets.py
@@ -2,6 +2,7 @@
 Reference:
 https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
 """
+from . import PairIndexSet
 import numpy as np
 import pandas as pd
 from os.path import join as path_join
@@ -9,6 +10,7 @@ from PIL import Image
 from torch.utils.data import Dataset
 from torch.utils.data.sampler import BatchSampler
 import zipfile
+# from copy import deepcopy
 
 
 class DeepFashionDataset(Dataset):
@@ -20,7 +22,8 @@ class DeepFashionDataset(Dataset):
         2. apply valid transformation
 
     Example:
-        train_ds = DeepFashionDataset("./deepfashion_data")
+        trans = Compose([Resize((224,224)), ToTensor()])
+        train_ds = DeepFashionDataset("./deepfashion_data", transform=trans)
         loader = DataLoader(train_ds)
     """
 
@@ -99,20 +102,45 @@ class DeepFashionDataset(Dataset):
                 img = self.transform(img)
 
         return (img, target)
+
+    @property
+    def unique_classes(self):
+        """
+        Return a (copyed) list of unique classes
+        """
+        if not hasattr(self, '_unique_classes'):
+            # build when we don't have
+            self._unique_classes = self.data['label'].unique()
+
+        ret = list(self._unique_classes)
+        print(len(ret))
+        return ret
+
+    def get_label_to_idxs(self, label):
+        if not hasattr(self, '_label_to_idxs'):
+            inv_map = {}
+            for k, v in self.data['label'].items():
+                inv_map[v] = inv_map.get(v, [])
+                inv_map[v].append(k)
+            self._label_to_idxs = inv_map
+
+        ret = self._label_to_idx[label]
+
+        return ret
 # -------------------------------------------------------------------------------
 
 
-class Pairize(Dataset):
+class Siamesize(Dataset):
     """
-    To make a dataset return pairs from another dataset
+    To make a dataset return pairs from another dataset for Siamese training
 
     Notes:
         This class is working in progress
 
     Example:
         ds = DeepFashionDataset(...)
-        paired_ds = Pairize(ds)
-        (img1, img2), (t1, t2) = paired_ds[0]
+        siamese_ds = Siamesize(ds)
+        (img1, img2), (t1, t2), y = siamese_ds[0]
     """
 
     # The random seed for selecting pairs in the mode of validation / testing
@@ -130,17 +158,76 @@ class Pairize(Dataset):
         # ----------------
         # pair index set
         # ----------------
-        self._pair_idx_set = set()
+        self._pair_idx_set = PairIndexSet()
 
         # ----------------
         # Random state
         # ----------------
         if self.train:
             # just alias the numpy random module
-            self.random_state = np.random
+            self.random_state = np.random  # enable to set a gobal seed
         else:
             # Use our random state container to provide sampling function
             self.random_state = np.random.RandomState(self._eval_seed)
+            self._build_eval_pairs()
+
+    def _build_eval_pairs(self):
+        """
+        To build evaluation dataset which is balanced
+        """
+        pass
+
+    def __getitem__(self, idx):
+        """
+        """
+        pass
+
+    def _get_item_in_train_mode(self, idx):
+        """
+        Args:
+            idx (int):
+
+        Outline:
+            0.
+            1. Select the pair with (idx, idx_2), where idx_2 is randomly selected
+            2. Check if already sampled
+            3. Goto 1 if sampled already, but limit the number of trials
+            4. When the number of trial > sqrt(the size of the dataset), clean the PairIndexSet
+            5. return (img1, img2), (c1, c2, target)
+        """
+        # randomly choose if the classes of the training pair are the same or not.
+        target = self.random_state.choice([0, 1])
+
+        # take the img1 and c1 with idx from self._dataset
+        img1, c1 = self._dataset[idx]
+
+        # draw the second data
+        n_trial = 1000
+        for t in range(n_trial):
+            if target == 1:
+                idxs = self._dataset.get_label_to_idxs(c1)
+                # Therefore, the class of self._dataset[idxs] are c1
+                idx2 = self.random_state.choice(idxs)
+            else:
+                # if target == 0 then we select the class first
+                classes = self._dataset.unique_classes.remove(c1)
+                c2 = self.random_state.choice(classes)
+                idxs = self._dataset.get_label_to_idxs(c2)
+                idx2 = self.random_state.choice(idxs)
+
+            if (idx, idx2) not in self._pair_idx_set:
+                break
+        if t < n_trial:
+            print(t, idx, idx2)
+            img2, c2 = self._dataset[idx2]
+            return (img1, img2), (c1, c2, target)
+        else:
+            print("Going to clean the cache and try resample it again")
+            return self._get_item_in_train_mode(idx)
+    # --------------------------------------------------------------------------
+
+    def __len__(self):
+        return len(self._dataset)
 
     def _save_pair(self, idx_pair):
         pass
@@ -223,79 +310,6 @@ class SiameseMNIST(Dataset):
             img1 = self.transform(img1)
             img2 = self.transform(img2)
         return (img1, img2), target
-
-    def __len__(self):
-        return len(self.mnist_dataset)
-
-
-class TripletMNIST(Dataset):
-    """
-    Train: For each sample (anchor) randomly chooses a positive and negative samples
-    Test: Creates fixed triplets for testing
-    """
-
-    def __init__(self, mnist_dataset):
-        self.mnist_dataset = mnist_dataset
-        self.train = self.mnist_dataset.train
-        self.transform = self.mnist_dataset.transform
-
-        if self.train:
-            self.train_labels = self.mnist_dataset.train_labels
-            self.train_data = self.mnist_dataset.train_data
-            self.labels_set = set(self.train_labels.numpy())
-            self.label_to_indices = {label: np.where(self.train_labels.numpy() == label)[0]
-                                     for label in self.labels_set}
-
-        else:
-            self.test_labels = self.mnist_dataset.test_labels
-            self.test_data = self.mnist_dataset.test_data
-            # generate fixed triplets for testing
-            self.labels_set = set(self.test_labels.numpy())
-            self.label_to_indices = {label: np.where(self.test_labels.numpy() == label)[0]
-                                     for label in self.labels_set}
-
-            random_state = np.random.RandomState(29)
-
-            triplets = [[i,
-                         random_state.choice(
-                             self.label_to_indices[self.test_labels[i].item()]),
-                         random_state.choice(self.label_to_indices[
-                             np.random.choice(
-                                 list(
-                                     self.labels_set - set([self.test_labels[i].item()]))
-                             )
-                         ])
-                         ]
-                        for i in range(len(self.test_data))]
-            self.test_triplets = triplets
-
-    def __getitem__(self, index):
-        if self.train:
-            img1, label1 = self.train_data[index], self.train_labels[index].item(
-            )
-            positive_index = index
-            while positive_index == index:
-                positive_index = np.random.choice(
-                    self.label_to_indices[label1])
-            negative_label = np.random.choice(
-                list(self.labels_set - set([label1])))
-            negative_index = np.random.choice(
-                self.label_to_indices[negative_label])
-            img2 = self.train_data[positive_index]
-            img3 = self.train_data[negative_index]
-        else:
-            img1 = self.test_data[self.test_triplets[index][0]]
-            img2 = self.test_data[self.test_triplets[index][1]]
-            img3 = self.test_data[self.test_triplets[index][2]]
-
-        img1 = Image.fromarray(img1.numpy(), mode='L')
-        img2 = Image.fromarray(img2.numpy(), mode='L')
-        img3 = Image.fromarray(img3.numpy(), mode='L')
-        if self.transform is not None:
-            img1 = self.transform(img1)
-            img2 = self.transform(img2)
-            img3 = self.transform(img3)
-        return (img1, img2, img3), []
 
     def __len__(self):
         return len(self.mnist_dataset)
