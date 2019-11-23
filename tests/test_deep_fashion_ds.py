a02036cb51ddfbed66fa1d80ae8ff8f465461ca3
Chris Lin <chris4540@gmail.com>
2019-11-23 20:31:08 +0100

    Add more test

--- tests/test_deep_fashion_ds.py
+++ tests/test_deep_fashion_ds.py
@@ -1,7 +1,27 @@
 import numpy as np
 from utils.datasets import DeepFashionDataset
 from PIL import Image
+from torchvision.transforms import ToTensor
+from torchvision.transforms import Compose
+from torchvision.transforms import Resize
+from torch.utils.data import Subset
+
 deep_fashion_root_dir = "./deepfashion_data"
+
+
+##################
+# Helper function
+##################
+def assert_dataset(dataset_, n_test=3):
+    for i in np.random.choice(len(dataset_), n_test):
+        # Get one item
+        im, target = dataset_[i]
+
+        # check img
+        assert isinstance(im, np.ndarray)
+
+        # Check class label
+        assert target >= 0
 
 
 def test_ds_len():
@@ -18,12 +38,15 @@ def test_ds_len():
 
 def test_get_item_from_ds():
     train_ds = DeepFashionDataset(deep_fashion_root_dir, 'train')
+    assert_dataset(train_ds)
 
-    # Get one item
-    im, target = train_ds[100]
+def test_subset_from_ds():
+    train_ds = DeepFashionDataset(deep_fashion_root_dir, 'train')
+    trans = Compose([Resize((224, 224)), ToTensor()])
+    n_samples = 20
+    select_idx = np.random.choice(len(train_ds), n_samples, replace=False)
+    sub_ds = Subset(train_ds, select_idx)
+    assert len(sub_ds) == n_samples
+    assert_dataset(sub_ds)
 
-    # check img
-    assert isinstance(im, np.ndarray)
 
-    # Check class label
-    assert target => 0
