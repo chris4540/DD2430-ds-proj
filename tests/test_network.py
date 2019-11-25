a5a9ffc2c8e1de82235d32bbca2ea01ef9dec3e1
Chris Lin <chris4540@gmail.com>
2019-11-25 17:12:21 +0000

    Update test

--- tests/test_network.py
+++ tests/test_network.py
@@ -1,14 +1,27 @@
 import torch
 from network.resnet import ResidualEmbNetwork
+from network.resnet import ResidualNetwork
+import torch.nn.functional as F
+
+
+def test_resnet16_emb():
+    batch_size = 5
+    input_img = torch.randn((batch_size, 3, 224, 224))
+    net = ResidualEmbNetwork()
+    emb_vec = net(input_img)
+    assert emb_vec.shape[0] == batch_size
+    assert emb_vec.shape[1] > 0
+    assert emb_vec.shape[1] == net.emb_dim
+
 
 def test_resnet16():
-	batch_size = 5
-	input_img = torch.randn((batch_size, 3, 224, 224))
-	net = ResidualEmbNetwork()
-	emb_vec = net(input_img)
-	print(emb_vec.shape)
-	assert emb_vec.shape[0] == batch_size
-	assert emb_vec.shape[1] > 0
-	assert emb_vec.shape[1] == net.emb_dim
+    batch_size = 5
+    n_cls = 10
+    input_img = torch.randn((batch_size, 3, 224, 224))
+    net = ResidualNetwork(nb_classes=n_cls)
+    out = net(input_img)
 
-
+    target = torch.randint(n_cls, (batch_size,), dtype=torch.int64)
+    loss = F.cross_entropy(out, target)
+    loss.backward()
+    assert loss > 0
