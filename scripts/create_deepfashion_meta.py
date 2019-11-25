8dd075ef47b4d27a8f2be37fe990e83180a249ee
Chris Lin <chris4540@gmail.com>
2019-11-25 17:11:35 +0000

    Update script

--- scripts/create_deepfashion_meta.py
+++ scripts/create_deepfashion_meta.py
@@ -16,6 +16,8 @@ Dataset webpage:
 from os.path import join as path_join
 import pandas as pd
 # ------------------------------
+
+
 class Config:
     data_root = "deepfashion_data"
     fulldata_csv_fname = 'full_data_meta.csv'
@@ -50,27 +52,31 @@ if __name__ == '__main__':
         images = []
         for i, line in enumerate(f.readlines()):
             if i > 1:
-                images.append([word.strip() for word in line.split(' ') if len(word) > 0])
+                images.append([word.strip()
+                               for word in line.split(' ') if len(word) > 0])
 
-    #get train, valid, test split
+    # get train, valid, test split
     file = path_join(cfg.data_root, "list_eval_partition.txt")
     with open(file, 'r') as f:
         images_partition = []
         for i, line in enumerate(f.readlines()):
             if i > 1:
-                images_partition.append([word.strip() for word in line.split(' ') if len(word) > 0])
+                images_partition.append(
+                    [word.strip() for word in line.split(' ') if len(word) > 0])
     # ---------------------------------------------------------------------------------------------------
     ############################################################
     # Construct the pandas dataframe with the data loaded
     ############################################################
     img_df = pd.DataFrame(images, columns=['images', 'category_label'])
-    partition_df = pd.DataFrame(images_partition, columns=['images', 'dataset'])
+    partition_df = pd.DataFrame(
+        images_partition, columns=['images', 'dataset'])
 
     # category_label: str -> int
     img_df = img_df.astype({'category_label': 'int32'})
 
     data_df = img_df.merge(partition_df, on='images')
-    data_df['category'] = data_df['category_label'].apply(lambda x: categories[x - 1])
+    data_df['category'] = data_df['category_label'].apply(
+        lambda x: categories[x - 1])
 
     print("---------- Metadata Summary ----------")
     print("Dataset split:")
@@ -83,7 +89,7 @@ if __name__ == '__main__':
     classes.sort()
 
     # build a mapping from category to index
-    cat_to_idx = {k:i for i, k in enumerate(classes)}
+    cat_to_idx = {k: i for i, k in enumerate(classes)}
     # map category_label to label (index)
     data_df['label'] = data_df['category'].map(cat_to_idx)
     # ---------------------------------------------------------------------------------
@@ -97,7 +103,8 @@ if __name__ == '__main__':
     ############################################################
     col = 'category'
     # filter the categories of which the number is less than the min_nb_cnt
-    filtered_df = data_df.groupby(col).filter(lambda x : len(x) > cfg.min_nb_cnt)
+    filtered_df = data_df.groupby(col).filter(
+        lambda x: len(x) > cfg.min_nb_cnt)
     # select the number of data to be sampled
     n_sample_data = filtered_df[col].value_counts().min()
     # re-sample of the column
@@ -108,11 +115,21 @@ if __name__ == '__main__':
     print("------ Sampled Metadata Summary ------")
     print("Class distribution")
     print(sampled_df[col].value_counts())
+    nclasses = sampled_df['category'].nunique()
+    print("# of classes: ", nclasses)
     print("--------")
     print("Dataset split:")
     print(sampled_df['dataset'].value_counts())
     print("--------------------------------------")
-    # save the sampled data as  csv
+    # Rebuild class index
+    classes = sampled_df['category'].unique()
+    classes.sort()
+    # build a mapping from category to index
+    cat_to_idx = {k: i for i, k in enumerate(classes)}
+    # map category_label to label (index)
+    sampled_df['label'] = sampled_df['category'].map(cat_to_idx)
+    # -----------------------------------
+    # save the sampled data as csv
     file = path_join(cfg.data_root, cfg.sampled_data_csv_fname)
     print("Saving {} ....".format(file))
     sampled_df.to_csv(file, index=False)
