ed61772ac23a49b8a0560f4f1d56ffb36f6661ed
Chris Lin <chris4540@gmail.com>
2019-11-25 18:16:10 +0000

    Update config

--- config/deep_fashion.py
+++ config/deep_fashion.py
@@ -3,9 +3,13 @@ Configuration module for deep fashion dataset
 """
 root_dir = "./deepfashion_data"
 
+
 class DeepFashionConfig:
     root_dir = root_dir
     mean = [0.7464, 0.7155, 0.7043]
     std = [0.2606, 0.2716, 0.2744]
-    # sizes = (224, 224)
-    sizes = (28, 28)
+
+    # Image net mean and std
+    # mean = [0.485, 0.456, 0.406]
+    # std = [0.229, 0.224, 0.225]
+    sizes = (224, 224)
