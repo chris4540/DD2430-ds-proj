3bc5d4c85d18a3a2ac9be4eb68932ddb21445aad
Chris Lin <chris4540@gmail.com>
2019-12-01 23:24:56 +0100

    Update

---
+++ siam_cat_loop.py
@@ -0,0 +1,15 @@
+"""
+Playgroud to write one loop
+
+1. Wrtie one loop
+2. write training in a free-style with ignite
+3. Final code refactoring
+
+Ref:
+https://github.com/pytorch/ignite/blob/master/examples/fast_neural_style/neural_style.py
+
+# check it for how to write loss without final pass
+https://pytorch.org/ignite/quickstart.html#id1
+
+https://fam-taro.hatenablog.com/entry/2018/12/25/021346
+"""
