e801d59815b5f0e2fbfb6f013b6ad7118d1c00da
Chris Lin <chris4540@gmail.com>
2019-12-06 19:06:20 +0000

    Prepare to run siam cos

--- train_siamcos.py
+++ train_siamcos.py
@@ -4,7 +4,6 @@ from experiment.siamcos import SiameseCosDistanceWithCat
 margin = np.round(np.sqrt(0.2), decimals=4)
 exp = SiameseCosDistanceWithCat(
     "./exp_folders/exp_siamcos",
-    debug=True,
     lr=0.0005,
     weight_decay=1e-5,
     eta_min=1e-6,
