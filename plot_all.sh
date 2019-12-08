f3dd319fda1e477e64e63cea2ffa5fdd4a33acdb
Chris Lin <chris4540@gmail.com>
2019-12-08 11:45:04 +0000

    Add plot scripts

---
+++ plot_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+folders="
+exp_cat
+exp_siamcat_m2_100
+exp_siamcat_m2_1000
+exp_siamcos
+exp_siamese_m2_100
+exp_siamese_m2_1000
+"
+
+for f in $folders; do
+    exp_folder=exp_results/$f
+    echo "Processing : " $exp_folder
+    python plt_search_results.py $exp_folder
+    python plt_emb.py $exp_folder
+done
